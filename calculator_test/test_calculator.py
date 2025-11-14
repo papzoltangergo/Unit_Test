b81567c845fe6d40903c5416ae5411d608b829eb
papzoltangergo <pap.zoltan.gergo@diak.szbi-pg.hu>
2025-11-14 10:51:10 +0100

    .gitignore

--- calculator_test/test_calculator.py
+++ calculator_test/test_calculator.py
@@ -7,3 +7,22 @@ def test_sum():
     assert sum(-2, -3) == -5
     assert sum(0, 0) == 0
 
+def test_subtract():
+    assert subtract(10, 5) == 5
+    assert subtract(-1, -1) == 0
+    assert subtract(-5, -3) == -2
+    assert subtract(0, 0) == 0
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+    assert multiply(-1, 1) == -1
+    assert multiply(-2, -3) == 6
+    assert multiply(0, 10) == 0
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(-6, 2) == -3
+    assert divide(-9, -3) == 3
+    assert divide(5, 0) == "Error: Division by zero"
+    
+#python -m pytest .\test_calculator.py -v
